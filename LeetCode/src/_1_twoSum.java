c4775e69b5b454e4c25d865b03421da043c53595
akore <akore@LAPTOP-OUTOJ61F>
2020-01-31 11:35:17 +0530

    Nil

---
+++ LeetCode/src/_1_twoSum.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+public class _1_twoSum{
+	public int[] twoSum(int[] nums, int target){
+		   HashMap<Integer,Integer> hash = new HashMap<Integer,Integer>();
+		        for(int i = 0; i < nums.length; i++){
+
+		            Integer diff = (Integer)(target - nums[i]);
+		            if(hash.containsKey(diff)){
+		                int toReturn[] = {hash.get(diff), i};
+		                return toReturn;
+		            }
+
+		            hash.put(nums[i], i);
+
+		        }
+		        
+		        return null;
+		}
+}
