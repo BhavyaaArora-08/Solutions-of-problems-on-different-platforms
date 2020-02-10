75b46a3dc3a16fa2e021d3de9a4b1b027ed3bda2
akore <akore@LAPTOP-OUTOJ61F>
2020-02-11 03:38:56 +0530

    recursion

---
+++ Hackerank/src/The_Power_Sum.java
@@ -0,0 +1,29 @@
+
+public class The_Power_Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(find(1, 13, 0,2));
+	}
+	
+	public static int find(int idx,int x,int sum,int k)
+	{
+		
+		if(sum==x)
+			return 1;
+		
+		if(sum>x || idx>Math.sqrt(x))
+			return 0;
+		
+		
+		int x1=find(idx+1,x, sum+(int)Math.pow(idx, k),k);
+		int x2=find(idx+1, x, sum,k);
+		
+		
+		return x1+x2;
+		
+		
+		
+	}
+
+}
